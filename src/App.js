6bc5185407eaf92124f4cf93bf6209b8382602b2
appmarestaing marestaing <marestaing.noah@pusd.us>
2024-03-21 19:17:14 +0000

    setting up nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
+import Typography from '@mui/material/Typography';
+
+import Container from '@mui/material/Container';
+import { Box, Button } from '@mui/material';
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function App() {
   return (
-    <div>Hello</div>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            <img src="./app_logo.png"/>
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+               
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+            
+              
+          
+          
+          
+          
+
+          
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 
