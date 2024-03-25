ee6adf59eb85802c8c2da30c4218d63bb61de60f
appmarestaing marestaing <marestaing.noah@pusd.us>
2024-03-25 21:26:56 +0000

    updates for the home page

--- src/App.js
+++ src/App.js
@@ -4,72 +4,58 @@ import Toolbar from '@mui/material/Toolbar';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Typography from '@mui/material/Typography';
-
 import Container from '@mui/material/Container';
 import { Box, Button } from '@mui/material';
 import Page1 from './pages/page1';
 import Page2 from './pages/page2';
+import Link from '@mui/material/Link';
+import Page3 from './pages/page3';
 
-const pages = ['Page 1', 'Page 2', 'Page 3'];
+const buttonStyle = {
+  my: 2,
+  color: 'white', 
+  display: 'block' 
+}
 
 function App() {
   return (
     <>
-      <AppBar position="static">
+      <AppBar position="static" className="navigation_bar">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-          </Typography>
-         
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex' }}}>
+            <Link href="/">
+              <Button 
+                sx={buttonStyle}
               >
-                {page}
+                Page 1
               </Button>
-            ))}
-          </Box>
+            </Link>
+            <Link href="/page2">
+              <Button
+                sx={buttonStyle}
+              >
+                Page 2
+              </Button>
+            </Link>
+            <Link href="/page3">
+              <Button
+                sx={buttonStyle}
+              >
+                Page 3
+              </Button>
+            </Link>
             
-              
-          
-          
-          
-          
-
-          
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
-
-
-
-
-
-
       <BrowserRouter>
         <Routes>
           <Route path="/">
             <Route index element={<Page1 />} />
-            <Route path="blogs" element={<Page2 />} />
+            <Route path="page2" element={<Page2 />} />
+            <Route path="page3" element={<Page3 />} />
           </Route>
         </Routes>
       </BrowserRouter>
